80b5de1b00c70cd2d6c60493c237f7d7a696d552
Juliet-Ol <agutjuliet@gmail.com>
2022-01-09 01:12:07 +0300

    add js file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//create an object
+    
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+ //Display data from the object:
+ document.getElementById("demo").innerHTML = person.fullName(); 
